46a2eb2bf2acb76a8729d02f16acfaf80e099455
Tiger Peng <nytigeryxp@gmail.com>
2022-08-08 22:56:08 -0400

    Finish UI with score display, and first to 5 logic

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-game();
+let btns = document.querySelectorAll('button');
+let maxBtnWidth = 0;
 
-function game() {
-    let wins = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = getPlayerSelection();
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-
-        switch(result.charAt(4)) {
-            case 'w':
-                wins++;
-                break;
-            case 'l':
-                wins--;
-                break;
-        }
-        
-        console.log(result);
+btns.forEach(btn => {
+    if (btn.offsetWidth > maxBtnWidth) {
+        maxBtnWidth = btn.offsetWidth;
     }
+});
 
-    if (wins > 0) {
-        console.log("Congrats! You won!");
-    } else if (wins < 0) {
-        console.log("Welp, you lost.");
-    } else {
-        console.log("You tied.");
-    }
-}
+btns.forEach(btn => {
+    btn.style.width = maxBtnWidth + "px";
+});
+
+let rockbtn = document.querySelector('#rockbtn');
+let paperbtn = document.querySelector('#paperbtn');
+let scissorsbtn = document.querySelector('#scissorsbtn');
+
+rockbtn.addEventListener('click', () => {
+    let resultString = playRound("rock", computerPlay());
+    updateGameResult(resultString);
+    updateScore(resultString);
+});
+
+paperbtn.addEventListener('click', () => {
+    let resultString = playRound("paper", computerPlay());
+    updateGameResult(resultString);
+    updateScore(resultString);
+});
+scissorsbtn.addEventListener('click', () => {
+    let resultString = playRound("scissors", computerPlay());
+    updateGameResult(resultString);
+    updateScore(resultString);
+});
+
+let result = document.querySelector('.result');
+
+let playerScore = document.querySelector('#playerscore');
+let computerScore = document.querySelector('#computerscore');
 
 function computerPlay() {
     let rand = Math.floor(Math.random() * 3);
@@ -85,16 +94,39 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function getPlayerSelection() {
-    let input = prompt("Choose: Rock, Paper, or Scissors").toLowerCase();
-
-    while (input !== "rock" && input !== "paper" && input !== "scissors") {
-        input = prompt("Please enter a valid selection.").toLowerCase();
-    }
-
-    return input;
-}
-
 function capitalize(word) {
     return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
-} 
+}
+
+function updateGameResult(text) {
+    result.textContent = text;
+}
+
+function updateScore(text) {
+    switch(text.charAt(4)) {
+        case 'w':
+            playerScore.textContent = +playerScore.textContent + 1;
+            break;
+        case 'l':
+            computerScore.textContent = +computerScore.textContent + 1;
+            break;
+    }
+
+    checkGameOver();
+}
+
+function checkGameOver() {
+    if (playerScore.textContent === "5") {
+        updateGameResult("The player has won!");
+        resetScores();
+    }
+    if (computerScore.textContent === "5") {
+        updateGameResult("The computer has won!");
+        resetScores();
+    }
+}
+
+function resetScores() {
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+}
